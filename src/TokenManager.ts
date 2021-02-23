88b98095d6ae7d41ceabbfb0b116069e3eb232d8
Freddie Miller <fred.miller097@gmail.com>
2021-02-23 10:16:57 +0000

    server: created TokenManager class

---
+++ src/TokenManager.ts
@@ -0,0 +1,13 @@
+class TokenManager {
+    constructor(){
+        // connect to database
+    }
+
+    generateToken(userid: string){
+        // Generate a jwt and return it
+    }
+
+    verifyToken(token: string): boolean {
+        return false;
+    }
+}
