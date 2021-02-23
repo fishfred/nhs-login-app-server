f081a9662006ceab733b5ec8071938a89586e4e8
Freddie Miller <fred.miller097@gmail.com>
2021-02-23 10:20:22 +0000

    tests: updated to use mock redis module

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,6 +1,11 @@
 import request from "supertest";
 import app from "../src/app";
 
+beforeAll((done) => {
+    jest.mock("redis", () => jest.requireActual("redis-mock"));
+    done();
+})
+
 describe("GET /", () => {
     it("should return 200", (done) => {
         request(app).get("/").expect(200, done);
